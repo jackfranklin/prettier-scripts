1683e2576dabdc77fb5e74c4d99fca86483e1681
Jack Franklin <jack@jackfranklin.net>
2017-07-19 12:30:16 +0100

    Add `--help` and `--version` to write

--- bin/write.js
+++ bin/write.js
@@ -6,8 +6,21 @@ const { prepareCommand } = require('../src/prepare-command')
 const { execShellCommand } = require('../src/util')
 const { outputRunningInfo, displayWriteOutput } = require('../src/output')
 
+const { help } = require('../src/output')
+
 const run = () => {
   const { argv } = require('yargs')
+
+  if (argv.v || argv.version) {
+    console.log(require('../package.json').version)
+    return
+  }
+
+  if (argv.help) {
+    console.log(help)
+    return
+  }
+
   const args = processCommandLineArguments(argv)
   const { command, changedGitFileNames } = prepareCommand(
     Object.assign({}, args, { write: true })
